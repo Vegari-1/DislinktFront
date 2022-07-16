f4535831af5ef5747ff22bc96c18fe73cbddb090
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-07-16 19:07:22 +0200

    feat: Not Found page

--- dislinkt-fe/src/pages/AuthPage/AuthPage.tsx
+++ dislinkt-fe/src/pages/AuthPage/AuthPage.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import PrimaryButton from "../../components/atoms/PrimaryButton/PrimaryButton";
 
 import "./AuthPage.module.css";
 
 const AuthPage: React.FC = () => {
-  return (
-    <div>
-      <PrimaryButton text="Proba" />
-    </div>
-  );
+  return <div>Auth Page</div>;
 };
 
 export default AuthPage;
