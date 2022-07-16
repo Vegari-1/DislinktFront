49090ec8452379e99dd50b920e71d39905b13854
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-07-16 17:42:29 +0200

    feat: Add react-router-dom package and init AuthPage

--- dislinkt-fe/src/App.tsx
+++ dislinkt-fe/src/App.tsx
@@ -1,7 +1,17 @@
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth" />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
