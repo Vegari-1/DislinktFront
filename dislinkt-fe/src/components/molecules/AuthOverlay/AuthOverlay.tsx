a4542733c43f0f1dd3268ce0618810bb037f0023
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-07-17 14:48:38 +0200

    feat: created GhostButton Component

--- dislinkt-fe/src/components/molecules/AuthOverlay/AuthOverlay.tsx
+++ dislinkt-fe/src/components/molecules/AuthOverlay/AuthOverlay.tsx
@@ -1,9 +1,31 @@
+import React from "react";
+import GhostButton from "../../atoms/GhostButton/GhostButton";
 import classes from "./AuthOverlay.module.css";
 
-const AuthOverlay = () => {
+interface AuthOverlayProps {
+  overlayClass: string;
+  header: string;
+  text: string;
+  buttonText: string;
+  onClickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const AuthOverlay: React.FC<AuthOverlayProps> = ({
+  overlayClass,
+  header,
+  text,
+  buttonText,
+  onClickHandler,
+}) => {
   return (
     <div className={classes["overlay-container"]}>
-      <div className={classes["overlay"]}></div>
+      <div className={classes["overlay"]}>
+        <div className={`${classes["overlay-panel"]} ${classes[overlayClass]}`}>
+          <h1>{header}</h1>
+          <p>{text}</p>
+          <GhostButton text={buttonText} onClickHandler={onClickHandler} />
+        </div>
+      </div>
     </div>
   );
 };
--- dislinkt-fe/src/components/molecules/SignUpForm/SignUpForm.tsx
+++ dislinkt-fe/src/components/molecules/SignUpForm/SignUpForm.tsx
@@ -1,5 +1,22 @@
+import PrimaryButton from "../../atoms/PrimaryButton/PrimaryButton";
+import PrimaryInputField from "../../atoms/PrimaryInputField/PrimaryInputField";
+
+import classes from "./SignUpForm.module.css";
+
 const SignUpForm: React.FC = () => {
-  return <div></div>;
+  const onSubmitHandler = () => {
+    alert("Sign up");
+  };
+
+  return (
+    <form>
+      <h1 className={classes.label}>Sign In</h1>
+      <PrimaryInputField text="Name" type="text" />
+      <PrimaryInputField text="Email" type="email" />
+      <PrimaryInputField text="Password" type="password" />
+      <PrimaryButton text="Sign Up" onClickHandler={onSubmitHandler} />
+    </form>
+  );
 };
 
 export default SignUpForm;
