d6cbccf6f1f73a1a07743094d19e51d1b966c503
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-08-01 20:58:36 +0200

    feat: add redux saga to sign up

--- dislinkt-fe/src/store/slices/auth.ts
+++ dislinkt-fe/src/store/slices/auth.ts
@@ -1,21 +1,23 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { SignInPayload } from "../../models/slices/auth";
+import { SignInPayload, SignUpPayload } from "../../models/slices/auth";
 
 const initAuthSliceValues = {
-  isAuthenticated: "helo",
+  signInActive: false,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState: initAuthSliceValues,
   reducers: {
-    signIn(state, action: PayloadAction<SignInPayload>) {
-      state.isAuthenticated = "ola";
+    signIn(state, action: PayloadAction<SignInPayload>) {},
+    signUp(state, action: PayloadAction<SignUpPayload>) {},
+    goToSignIn(state) {
+      state.signInActive = true;
     },
   },
 });
 
-export const { signIn } = authSlice.actions;
+export const { signIn, signUp, goToSignIn } = authSlice.actions;
 
 export default authSlice.reducer;
