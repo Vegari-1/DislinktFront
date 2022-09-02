be08a74c24f84800390aea573c401815294bb6f6
Eva Janković <evaj.evadesign@gmail.com>
2022-09-02 12:57:01 +0200

    feat: create register form design

--- dislinkt-fe/src/models/formik/FormikForm.ts
+++ dislinkt-fe/src/models/formik/FormikForm.ts
@@ -2,6 +2,7 @@ interface FormikForm {
   errors: any;
   touched: any;
   setFieldTouched: (name: string, touched: boolean) => void;
+  setFieldValue: (name: string, value: any) => void;
 }
 
 export default FormikForm;
