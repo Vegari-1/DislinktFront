6438f4db3cb931aff4af6d5e1de0472e061c65e1
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-09-04 22:21:03 +0200

    feat: create people page

---
+++ dislinkt-fe/src/pages/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,93 @@
+import ProfileCard from "../../components/atoms/ProfileCard/ProfileCard";
+import Header from "../../components/molecules/Header/Header";
+import Layout from "../../components/organisms/Layout/Layout";
+import classes from "./PeoplePage.module.css";
+
+const PeoplePage: React.FC = () => {
+  return (
+    <Layout>
+      <Header />
+      <div className={classes["streach"]}>
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+        <ProfileCard
+          id="347903f7-28d0-4dca-803d-3b0cc0e61e71"
+          name="Ksenija"
+          surname="Prcic"
+          username="ksenija"
+          picture="picture"
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default PeoplePage;
