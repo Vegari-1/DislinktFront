e559636b5bbb4eec277b819986de79696658fe58
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-09-04 22:21:59 +0200

    feat: add people page to sidebar

--- dislinkt-fe/src/components/atoms/SidebarHeader/SidebarHeader.tsx
+++ dislinkt-fe/src/components/atoms/SidebarHeader/SidebarHeader.tsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import classes from "./SidebarHeader.module.css";
 
 const SidebarHeader: React.FC = () => {
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    navigate("/home");
+  };
+
   return (
     <div className={classes["side-bar-header-wrapper"]}>
-      <div className={classes["logo"]} />
+      <div className={classes["logo"]} onClick={onClickHandler} />
       <span className={classes["logo-title"]}>Dislinkt</span>
     </div>
   );
