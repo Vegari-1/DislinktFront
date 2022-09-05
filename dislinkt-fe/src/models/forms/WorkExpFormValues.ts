f5412a6b511fd2ef972ee01a274bff9c404bd26b
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-09-05 23:53:54 +0200

    feat: create work expirience form modal

---
+++ dislinkt-fe/src/models/forms/WorkExpFormValues.ts
@@ -0,0 +1,8 @@
+interface WorkExpFormValues {
+  position: string;
+  company: string;
+  startDate: Date;
+  endDate?: Date;
+}
+
+export default WorkExpFormValues;
--- dislinkt-fe/src/pages/ProfileWorkPage/ProfileWorkPage.tsx
+++ dislinkt-fe/src/pages/ProfileWorkPage/ProfileWorkPage.tsx
@@ -2,18 +2,29 @@ import ProfilePane from "../../components/organisms/ProfilePane/ProfilePane";
 import Layout from "../../components/organisms/Layout/Layout";
 import classes from "./ProfileWorkPage.module.css";
 import ManageItems from "../../components/organisms/ManageItems/ManageItems";
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import Backdrop from "../../components/atoms/Backdrop/Backdrop";
+import WorkExpFormOverlay from "../../components/molecules/WorkExpFormOverlay/WorkExpFormOverlay";
 
 const ProfileWorkPage: React.FC = () => {
   // dobavi informacije o korisniku (koristi se i za pregled sopstvenog i za pregled profila drugih korisnika)
   // ako je korisnik koji gleda profil vlasnik profila, prikazi mu "Edit" i "Kanticu" za brisanje i "+" za dodavanje
   // ako korisnik nije vlasnik profila, prikazi dugme "Block" (AKO JE BLOKIRAO PROFIL, NE TREBA DA MU ISKACE?)
   // ako korisnik nije vlasnik profila i ne prati profil, prikazi dugme "Connect"
+  const [modalVisible, setModalVisible] = useState(false);
   const deleteWorkHandler = () => {
     console.log("deleted work");
   };
   const addWorkHandler = () => {
     console.log("added work");
+    setModalVisible(true);
   };
+  const onCloseHandler = () => {
+    console.log("ugasi modal");
+    setModalVisible(false);
+  };
+
   return (
     <Layout>
       <div className={classes["streach"]}>
@@ -46,6 +57,17 @@ const ProfileWorkPage: React.FC = () => {
           />
         </ProfilePane>
       </div>
+
+      {modalVisible &&
+        ReactDOM.createPortal(
+          <Backdrop onBackdropClick={onCloseHandler} blur />,
+          document.getElementById("backdrop-root")!
+        )}
+      {modalVisible &&
+        ReactDOM.createPortal(
+          <WorkExpFormOverlay onClose={onCloseHandler} />,
+          document.getElementById("modal-root")!
+        )}
     </Layout>
   );
 };
